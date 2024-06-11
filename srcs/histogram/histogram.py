d42fc3f62c125df67ea9759f81072e79f26b2ee8
kazourak <nskiba@student.42angouleme.fr>
2024-06-11 19:18:58 +0200

    add histogram and requirements.txt

--- srcs/histogram/histogram.py
+++ srcs/histogram/histogram.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 
 def histogram(df: pd.DataFrame, verbose: bool = False) -> pd.DataFrame:
-    return df
+
+    # Group by Hogwarts House and calculate the mean of all courses
+    return df.groupby('Hogwarts House')['Arithmancy'].mean()

031310abb791a2dacfa8a47838e754cce47f8bfa
farinaleo <leo.farina.fr@gmail.com>
2024-06-12 12:18:12 +0200

    :recycle: improve plot

--- srcs/scatter_plot/scatter_plot.py
+++ srcs/scatter_plot/scatter_plot.py
@@ -4,29 +4,30 @@ import sys
 import matplotlib.pyplot as plt
 
 
-def scatter_plot(df: pd.DataFrame, y_cols: list = None, verbose: bool = False) -> None:
+def scatter_plot(df: pd.DataFrame, x_col: list = None, y_col: list = None, verbose: bool = False) -> None:
     """
     Create a scatter subplot by plotting each column of the dataframe.
     :param df: the dataframe to plot.
-    :param y_cols: the list of columns to use as y. If not specified use all columns.
+    :param x_col: the list of columns to use as x. If not specified use all columns.
+    :param y_col: the list of columns to use as y. If not specified use all columns.
     :param verbose: print additional information.
     :return: None
     """
     df_tmp = df.drop(columns='Index', inplace=False)
-    x_cols = df_tmp.select_dtypes(include=['number']).columns.tolist()
-    y_cols = x_cols if y_cols is None else y_cols
+    x_col = df_tmp.select_dtypes(include=['number']).columns.tolist() if x_col is None else x_col
+    y_col = x_col if y_col is None else y_col
 
-    if verbose: print(f'Columns: {x_cols}')
+    if verbose: print(f'Columns: {x_col}')
 
     font = {'weight': 'light', 'size': 7}
     plt.rc('font', **font)
 
-    x_size, y_size = len(x_cols), len(y_cols)
+    x_size, y_size = len(x_col), len(y_col)
     fig, axes = plt.subplots(nrows=y_size, ncols=x_size, squeeze=False, sharex='col', sharey='row', figsize=(20, 20))
 
     for x in range(x_size):
         for y in range(y_size):
-            plot_single_scatter(df_tmp, x_cols, y_cols, axes[y, x], x, y)
+            plot_single_scatter(df_tmp, x_col, y_col, axes[y, x], x, y)
 
     plt.show()
 
@@ -61,12 +62,13 @@ def plot_single_scatter(df: pd.DataFrame, x_cols: list, y_cols: list, fig: plt.A
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('[ERROR] Usage: python scatter_plot.py <file name> <y_col1> ... <y_coln>')
+    if len(sys.argv) != 2 and len(sys.argv) != 4:
+        print('[ERROR] Usage: python scatter_plot.py <file name> <x_col> <y_col>')
         exit(1)
     try:
         df = open_csv(sys.argv[1])
-        scatter_plot(df, y_cols=sys.argv[2:] if len(sys.argv) > 2 else None, verbose=True)
+        scatter_plot(df, x_col=[sys.argv[2]] if len(sys.argv) > 2 else None,
+                     y_col=[sys.argv[3]] if len(sys.argv) > 2 else None, verbose=True)
     except Exception as e:
         print('[ERROR] Could not open the file properly.')
         print(e)

73644b1e23a094ec85c95effb84847ca8daf68d3
farinaleo <leo.farina.fr@gmail.com>
2024-06-13 13:14:36 +0200

    :construction: clean data, replace missing values, replace string with numeric values

--- srcs/logistic_regression/tools/format_csv.py
+++ srcs/logistic_regression/tools/format_csv.py
@@ -8,11 +8,12 @@ def format_csv(path: str, config: str = None, norm_data: bool = False, verbose: 
         if config is not None:
             df = apply_config(config, df, verbose=verbose)
         if norm_data:
+            df = add_missing_values(df)
             df = normalise_csv(df)
         return df
 
-    except Exception:
-        raise ValueError('[ERROR] Some thing went wrong with csv and/or config file.')
+    except Exception as e:
+        raise ValueError(f'[ERROR] Some thing went wrong with csv and/or config file.\n{e}')
 
 
 def open_csv(path: str) -> pd.DataFrame:
@@ -24,10 +25,35 @@ def apply_config(config: str, df: pd.DataFrame, verbose: bool = False) -> pd.Dat
     conf = configparser.ConfigParser()
     conf.optionxform = lambda option: option
     conf.read(config)
-    for col in conf['COLUMNS']:
-        if not bool(conf['COLUMNS'].getboolean(col)) and col in df.columns:
+    df = config_drop_columns(df, conf, verbose=verbose)
+    df = config_replace_values(df, conf, verbose=verbose)
+    return df
+
+
+
+def config_drop_columns(df: pd, config:configparser.ConfigParser, verbose:bool=False) -> pd.DataFrame:
+    for col in config['COLUMNS']:
+        if not bool(config['COLUMNS'].getboolean(col)) and col in df.columns:
             if verbose: print(f'[INFO] Drop {col}')
             df.drop(col, axis=1, inplace=True)
+    return df
+
+
+def config_replace_values(df: pd, config:configparser.ConfigParser, verbose:bool=False) -> pd.DataFrame:
+    for section in config.sections():
+        if section.startswith('REPLACE:'):
+            sub_section = section.replace('REPLACE:', '')
+            if sub_section in df.columns:
+                for var in config[section]:
+                    if verbose: print(f'[INFO] replace {var} by {config[section][var]} in {sub_section}')
+                    df[sub_section] = df[sub_section].replace(var, config[section][var])
+    return df
+
+
+def add_missing_values(df: pd.DataFrame) -> pd.DataFrame:
+    for col in df.select_dtypes(include=['number']).columns.tolist():
+        _mean = df[col].mean()
+        df[col] = df[col].fillna(_mean)
     return df
 
 

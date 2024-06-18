ee7f07ab5c4657da60ccc5d4290e44fa78f6ff05
kazourak <nskiba@student.42angouleme.fr>
2024-06-18 08:38:20 +0200

    refactor split function

--- srcs/logistic_regression/tools/split.py
+++ srcs/logistic_regression/tools/split.py
@@ -1,23 +1,22 @@
 import pandas as pd
 
 
-def split_date(data: pd.DataFrame, test_size: float, random_state: int):
+def split_date(data: pd.DataFrame, test_size: float, random_state: int) -> tuple:
     """
     Split the data into training and testing sets
     :param data: the data to split
-    :param test_size: the size of the testing set in percentage
+    :param test_size: the size of the testing set
     :param random_state: the seed for the random number generator
     :return: the training and testing sets X_train, X_test, y_train, y_test
     """
-    frac = test_size / 100.0
 
-    X_test = data.sample(frac=frac, random_state=random_state)
-    y_test = X_test['Hogwarts House']
+    data_sample = data.sample(frac=1, random_state=random_state)
 
-    X_index = X_test.index
+    X_train = data_sample.iloc[:int(len(data_sample) * (1 - test_size))]
+    X_test = data_sample.iloc[int(len(data_sample) * (1 - test_size)):]
 
-    X_train = data.drop(X_index)
     y_train = X_train['Hogwarts House']
+    y_test = X_test['Hogwarts House']
 
     X_train = X_train.drop(columns=['Hogwarts House'])
     X_test = X_test.drop(columns=['Hogwarts House'])

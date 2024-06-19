e8545e1aa47cfad028192691f5992b7647393adb
kazourak <nskiba@student.42angouleme.fr>
2024-06-19 08:59:54 +0200

    update sigmoid and gradient_descent

--- srcs/logistic_regression/gradient_descent.py
+++ srcs/logistic_regression/gradient_descent.py
@@ -4,24 +4,23 @@ from tools.sigmoid import sigmoid
 import matplotlib.pyplot as plt
 
 
-def gradient_descent(X: pd.Series, Y: pd.Series, theta: list, learning_rate: float, epoch: int) -> list:
+def gradient_descent(X: pd.Series, Y: pd.Series, learning_rate: float, epoch: int) -> dict[str, float]:
     """
     Compute the gradient descent of the logistic regression
     :param X: pd.Series, the feature
     :param Y: pd.Series, the target
-    :param theta: list, the theta
     :param learning_rate: float, the learning rate
     :param epoch: int, the number of iterations
-    :return: list, the updated thetas
+    :return: dict, the thetas
     """
     m = len(Y)
-    thetas = theta
+    thetas = {'theta0': 0, 'theta1': 0}
 
     for _ in range(epoch):
         z = X.apply(lambda x: thetas[0] + thetas[1] * x)
         h = sigmoid(z)
 
-        thetas[0] -= learning_rate * (1 / m) * np.sum(h - Y)
-        thetas[1] -= learning_rate * (1 / m) * np.sum(np.transpose(h - Y) * X)
+        thetas['theta0'] -= learning_rate * (1 / m) * np.sum(h - Y)
+        thetas['theta1'] -= learning_rate * (1 / m) * np.sum(np.transpose(h - Y) * X)
 
     return thetas

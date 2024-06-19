3e03c83760e1358758a7f0a0f6635e01b99cb368
farinaleo <leo.farina.fr@gmail.com>
2024-06-19 10:10:29 +0200

    :sparkles: start the main scrypt (mandatory)

---
+++ srcs/logistic_regression/logreg_train.py
@@ -0,0 +1,35 @@
+from tools.format_csv import format_csv
+from train import train_model
+import argparse
+
+
+def options_parser():
+    """Use to handle program parameters and options.
+    """
+    parser = argparse.ArgumentParser(
+        prog='DSLR train model',
+        description='this program should be used to train a model of logistic regression able to predict this '
+                    'hogwarts house of a student.',
+        epilog='Please read the subject before proceeding to understand the input file format.')
+    parser.add_argument('Train_file', type=str, nargs=1)
+    parser.add_argument('-v', '--verbose', action='store_true', help='show the the results step by step in an GUI')
+    parser.add_argument('-c', '--config', type=str, default='../../data/logistic.ini',
+                        help='The config file.')
+    parser.add_argument('-l', '--learning_rate', type=float, default=0.1, help='The learning rate of the gradient '
+                                                                               'descent.')
+    parser.add_argument('-e', '--epoch', type=int, default=1000, help='The number of iterations of the '
+                                                                      'gradient descent.')
+    parser.add_argument('-y', '--y_col', type=str, default='Hogwarts House', help='The columns to predict.')
+    return parser
+
+
+if __name__ == '__main__':
+    try:
+        args = options_parser().parse_args()
+        df = format_csv(args.Train_file[0], config=args.config, verbose=args.verbose)
+        print(df.columns)
+        model = train_model(df, args.y_col, args.learning_rate, args.epoch)
+        print(model)
+    except Exception as e:
+        print('[ERROR] The training process failed')
+        print(e)

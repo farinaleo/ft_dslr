1b4384d6b451a395ee0a228b6135ba933ffbb64f
farinaleo <leo.farina.fr@gmail.com>
2024-06-19 10:07:45 +0200

    :sparkles: add function to norm and unnorm data and thetas

---
+++ srcs/logistic_regression/tools/norm.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+def normalise_df(df: pd.Series) -> pd.Series:
+    """
+    Normalise a dataframe to obtain a μ = 0 and σ = 1 with the Z-score.
+    :param df: The dataframe to normalise.
+    :return: The normalised dataframe.
+    """
+
+    df_cp = df.copy(deep=True)
+
+    _mean = df_cp.mean()
+    _std = df_cp.std()
+    df_cp = df_cp.apply(lambda x: (x - _mean) / _std)
+
+    return df_cp
+
+
+def denorm_thetas(thetas: list[float], mean_x: float, std_x: float, mean_y: float, std_y: float) -> list[float]:
+    """
+    Denormalise thetas trained on a normalised dataframe.
+    :param std_y: the standard deviation of the data to predict.
+    :param mean_y: the mean of the data to predict.
+    :param std_x: the standard deviation of the explain data.
+    :param mean_x: the standard deviation of the explain data.
+    :param thetas: normalised thetas.
+    :return: Denormalised thetas.
+    """
+    t1 = thetas[1] * (std_y / std_x)
+    t0 = mean_y + std_y * (thetas[0] - (thetas[1] * (mean_x / std_x)))
+
+    return [t0, t1]

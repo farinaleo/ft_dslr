206caa7e38b180f0f28e1c0ca3d19fb3052a6666
farinaleo <leo.farina.fr@gmail.com>
2024-06-19 10:09:50 +0200

    :sparkles: start the training process

---
+++ srcs/logistic_regression/train.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from gradient_descent import gradient_descent
+
+
+def train_model(df: pd.DataFrame, y_col: str, learning_rate: float = 0.1, epoch: int = 1000) -> dict:
+    model = {}
+
+    return model

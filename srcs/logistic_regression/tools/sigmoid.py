e099786d74237a6dd88436a2b4c5e16348f15ee3
kazourak <nskiba@student.42angouleme.fr>
2024-06-19 08:59:54 +0200

    update sigmoid and gradient_descent

--- srcs/logistic_regression/tools/sigmoid.py
+++ srcs/logistic_regression/tools/sigmoid.py
@@ -5,7 +5,7 @@ import pandas as pd
 def sigmoid(z: pd.Series) -> pd.Series:
     """
     Compute the sigmoid function
-    :param z: float, the input
-    :return: float, the sigmoid of z
+    :param z: pd.Series, the input
+    :return: pd.Series, the output
     """
     return z.apply(lambda x: 1 / (1 + np.exp(-x)))

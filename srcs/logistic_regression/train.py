48d9b53968dd268951e2466f277085ea481a2976
farinaleo <leo.farina.fr@gmail.com>
2024-06-20 10:54:32 +0200

    :memo: comment code

--- srcs/logistic_regression/train.py
+++ srcs/logistic_regression/train.py
@@ -5,6 +5,14 @@ from tqdm import tqdm
 
 
 def train_model(X: pd.DataFrame, y: pd.Series, learning_rate: float = 0.1, epoch: int = 1000) -> dict:
+    """
+    Train a logistic regression model on the X and y data.
+    :param X: The explanatory variables.
+    :param y: The variable to predict.
+    :param learning_rate: The learning rate applied to the gradient descent.
+    :param epoch: The number of iterations to train the model.
+    :return: A dictionary containing the training model for each explanatory variables.
+    """
     model = {}
 
     with tqdm(total=len(X.columns) * 4, desc="Training ", ncols=100) as pbar:
@@ -17,6 +25,15 @@ def train_model(X: pd.DataFrame, y: pd.Series, learning_rate: float = 0.1, epoch
 
 
 def learn_multiple_y(X: pd.Series, y: pd.Series, epoch: int, learning_rate: float, pbar=None) -> dict:
+    """
+    Apply for each variable to predict, in the y dataset, the gradient descent.
+    :param X: The explanatory variables.
+    :param y: The variable to predict.
+    :param epoch: The number of iterations to train the model.
+    :param learning_rate: The learning rate applied to the gradient descent.
+    :param pbar: The progress bar to update.
+    :return: a dictionary containing the training model for each variable to predict.
+    """
     model = {}
     y = y.astype(int)
     params = y.unique()
@@ -25,8 +42,7 @@ def learn_multiple_y(X: pd.Series, y: pd.Series, epoch: int, learning_rate: floa
         _X = X.copy(deep=True)
         _y = y.replace(params, [1 if e == param else 0 for e in params])
         model[str(param)] = gradient_descent(_X, _y, epoch=epoch, learning_rate=learning_rate)
-        pbar.update(1)
+        if pbar is not None:
+            pbar.update(1)
 
     return model
-
-

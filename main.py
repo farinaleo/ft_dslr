04fa2cf48b23306fd66d4cb568be74c4f0b45cdf
kazourak <nskiba@student.42angouleme.fr>
2024-06-20 13:21:06 +0200

    refactor predict function and export result to a csv

--- main.py
+++
@@ -1,3 +0,0 @@
-from srcs.logistic_regression.logreg_predict import logreg_predict
-
-logreg_predict("data/dataset_train.csv", "data/poids.json")
--- srcs/logistic_regression/logreg_predict.py
+++ srcs/logistic_regression/logreg_predict.py
@@ -1,47 +1,68 @@
 import pandas as pd
 import json
 import configparser
-from .tools.format_csv import config_drop_columns, format_csv
+from .tools.format_csv import config_drop_columns, format_csv, list_to_csv
 import numpy as np
 from sklearn.metrics import accuracy_score
 
-def logreg_predict(data: str, weight: str):
-    df_data = format_csv(data, config="data/logistic.ini", norm_data=True)
 
-    with open(weight) as json_data:
-        weight = json.load(json_data)
+def load_weights(weight_path: str):
+    """
+    Load the weights from the json file
+    :param str, weight_path: the path to the weights
+    :return: the weights in a dictionary
+    """
+    with open(weight_path) as json_data:
+        return json.load(json_data)
 
+
+def load_config(config_path: str):
+    """
+    Load the configuration file
+    :param str, config_path: the path to the configuration file
+    :return: the configuration in a ConfigParser object
+    """
     conf = configparser.ConfigParser()
     conf.optionxform = lambda option: option
-    conf.read("data/logistic.ini")
+    conf.read(config_path)
+    return conf
 
-    df_data = config_drop_columns(df_data, conf, verbose=False)
+
+def prepare_data(df_data: pd.DataFrame, config: configparser.ConfigParser):
+    """
+    Prepare the data for the prediction
+    :param pd.DataFrame, df_data: the data
+    :param configparser.ConfigParser, config: the configuration
+    :return: the data and the target
+    """
+    df_data = config_drop_columns(df_data, config, verbose=False)
     X = df_data['Hogwarts House'].copy(deep=True)
     df_data.drop('Hogwarts House', axis=1, inplace=True)
+    return df_data, X
 
+
+def predict(df_data: pd.DataFrame, weights: dict):
     predict_list = []
 
     for index, row in df_data.iterrows():
-        _total = {0: 0,
-                  1: 0,
-                  2: 0,
-                  3: 0}
+        _total = {0: 0, 1: 0, 2: 0, 3: 0}
         for key, value in row.items():
-            if key in weight:
-                for house, thetas in weight[key].items():
-                    if not pd.isnull(value) and type(value) == float:
-                        _total[int(house)] += float(1 / (1 + np.exp(-(float(thetas["theta0"]) + float(thetas["theta1"]) * float(value)))))
+            if key in weights:
+                for house, thetas in weights[key].items():
+                    if not pd.isnull(value) and isinstance(value, float):
+                        _total[int(house)] += float(
+                            1 / (1 + np.exp(-(float(thetas["theta0"]) + float(thetas["theta1"]) * float(value)))))
         predict_list.append((index, _total))
+    return predict_list
 
+
+def predict_house(predict_list: list):
+    """
+    Predict the house of the students
+    :param list, predict_list: the list of predictions
+    :return: Predicted labels
+    """
     _y_pred = []
-    _y_true = []
-
-
-    print(len(predict_list))
-    print(len(df_data.columns))
-    print(len(df_data))
-    print()
-    cnt_true = 0
     for i, e in predict_list:
         _max = float('-inf')
         _house = None
@@ -49,15 +70,45 @@ def logreg_predict(data: str, weight: str):
             if e[house] > _max:
                 _max = e[house]
                 _house = house
-        print(f'predict house {_house} - {X.iloc[i]} | {int(_house) == int(X.iloc[i])}: {e}')
-        if int(_house) == int(X.iloc[i]):
-            cnt_true += 1
         _y_pred.append(int(_house))
-        _y_true.append(int(X.iloc[i]))
-
-    print()
-    print(f'{cnt_true} / {len(predict_list)} | {(cnt_true / len(predict_list)) * 100} % de prediction')
+    return _y_pred
 
 
+def category_to_label(y: list, conf: configparser.ConfigParser):
+    """
+    Convert categories to string labels
+    :param list, y: the binary labels
+    :param conf: the configuration
+    :return: predicted house labels
+    """
+    y_labels = []
 
-    print(accuracy_score(_y_true, _y_pred))
+    house_dict = dict(conf['REPLACE:Hogwarts House'])
+    reversed_dict = {value: key for key, value in house_dict.items()}
+
+    for i in y:
+        y_labels.append(reversed_dict[str(i)])
+
+    return y_labels
+
+
+def logreg_predict(data: str, weight: str):
+    """
+    Predict the house of the students
+    :param str, data: the path to the dataset
+    :param str, weight: the path to the weights
+    :return:
+    """
+    config_path = "data/logistic.ini"
+    df_data = format_csv(data, config=config_path, norm_data=True)
+    weights = load_weights(weight)
+    config = load_config(config_path)
+
+    df_data, X = prepare_data(df_data, config)
+    predict_list = predict(df_data, weights)
+
+    y_pred = predict_house(predict_list)
+
+    pred_labels = category_to_label(y_pred, config)
+
+    list_to_csv(pred_labels)

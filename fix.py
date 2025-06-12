49b32ced50dde27237249cf29b1588ea1ad39c95
nskiba-sub <nolan.skiba@student.42angouleme.fr>
2025-06-12 11:25:30 +0200

    fix

---
+++ fix.py
@@ -0,0 +1,8 @@
+# Bonjour Leo, j'ai besoin de ton aide pour corriger ce code Python.
+# Il y a 3 erreurs dans la codebase.
+# Malheuresement, je ne peux pas trouver les erreurs sans l'aide de mon ami, Leo.
+# De plus, il est actuellement 11h24 et il sera 11h27 au moment où tu liras ce message.
+# Tu ne voudrais pas aller manger et, ensuite, revenir pour corriger le code, n'est-ce pas ?
+# Merci pour ton aide, Leo !
+
+# Amicalement, Nolan
